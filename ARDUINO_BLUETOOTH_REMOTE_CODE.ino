3e474d9bdc64c3251c3c2f1cf09b38ade6e11c9e
K SOLOMON JONES <ksjones777@gmail.com>
2016-09-01 07:14:49 +0530

    Add files via upload

---
+++ ARDUINO_BLUETOOTH_REMOTE_CODE.ino
@@ -0,0 +1,55 @@
+int inByte;
+void setup()
+{
+  Serial.begin(9600);
+  pinMode(9, OUTPUT);
+  pinMode(10, OUTPUT);
+  pinMode(11, OUTPUT);
+  pinMode(12, OUTPUT);
+}
+void loop()
+{
+  if (Serial.available() > 0)
+  {
+  inByte = Serial.read();
+  switch (inByte)
+  {
+    case '1':
+      digitalWrite(9, LOW);
+      digitalWrite(10, HIGH);
+      digitalWrite(11, LOW);
+      digitalWrite(12, HIGH);
+      Serial.println("FRONT");
+
+      break;
+    case '2':
+      digitalWrite(9, HIGH);
+      digitalWrite(10, LOW);
+      digitalWrite(11, HIGH);
+      digitalWrite(12, LOW);
+      Serial.println("BACK");
+      break;
+    case '3':
+      digitalWrite(9, LOW);
+      digitalWrite(10, LOW);
+      digitalWrite(11, LOW);
+      digitalWrite(12, HIGH);
+      Serial.println("LEFT");
+      break;
+    case '4':
+      digitalWrite(9, LOW);
+      digitalWrite(10, HIGH);
+      digitalWrite(11, LOW);
+      digitalWrite(12, LOW);
+      Serial.println("RIGHT");
+      break;
+    case '5':
+      digitalWrite(9, LOW);
+      digitalWrite(10, LOW);
+      digitalWrite(11, LOW);
+      digitalWrite(12, LOW);
+      Serial.println("STOP");
+      break;
+  }
+}}
+
